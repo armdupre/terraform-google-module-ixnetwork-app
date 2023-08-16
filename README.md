d6c750e232edd8af50c86ac74cc53724b8233a84
armdupre <a-joseph.dupre@keysight.com>
2023-08-16 16:53:09 -0400

    Updated.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # module-ixnetwork-app/google
+
+## Description
+Terraform module for IxNetwork application deployment on Google Cloud Platform
+
+## Deployment
+This module creates a single instance having a single network interface.
+
+## Usage
+```tf
+module "App" {
+	source  = "armdupre/module-ixnetwork-app/google"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+}
+```
--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "Eth0PublicIpAddress" {
+	description = "Public ip address resource associated with the first network interface"
 	value = {
 		"address" : google_compute_address.Eth0PublicIpAddress.address
 		"address_type" : google_compute_address.Eth0PublicIpAddress.address_type
@@ -6,6 +7,7 @@ output "Eth0PublicIpAddress" {
 }
 
 output "Image" {
+	description = "Image resource associated with the virtual machine"
 	value = {
 		"family" : data.google_compute_image.Image.family
 		"name" : data.google_compute_image.Image.name
@@ -14,6 +16,7 @@ output "Image" {
 }
 
 output "Instance" {
+	description = "Instance resource associated with the virtual machine"
 	value = {
 		"network_ip" : google_compute_instance.Instance.network_interface.0.network_ip 
 	}
