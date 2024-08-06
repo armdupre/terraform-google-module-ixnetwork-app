5f59c6547cd4cc130fdcef3602837de22316ffea
armdupre <a-joseph.dupre@keysight.com>
2024-08-05 23:45:53 -0400

    Updated.

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
 locals {
 	CustomImageProjectId = var.CustomImageProjectId
 	CustomImageFamilyName = var.CustomImageFamilyName
-	Eth0PublicIpAddressName = replace("${local.Preamble}-eth0-ip-addr", "_", "-")
+	Eth0PublicIpAddressName = "${local.Preamble}-eth0-ip-addr"
 	Eth0SubnetName = var.Eth0SubnetName
 	Eth0VpcNetworkName = var.Eth0VpcNetworkName
 	InstanceId = var.InstanceId
 	InstanceName = "${local.Preamble}-instance"
 	MachineType = var.MachineType
 	NetworkTargetTags = var.NetworkTargetTags
-	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.Tag}-${local.Version}-${local.InstanceId}"
+	Preamble = replace("${local.UserLoginTag}-${local.UserProjectTag}-${local.Tag}-${local.Version}-${local.InstanceId}", "_", "-")
 	RegionName = var.RegionName
 	Tag = var.Tag
 	UserEmailTag = var.UserEmailTag
